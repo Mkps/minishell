ea9443f9282608d01c0e90f96c36e39c09f28285
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-09-11 22:01:59 +0200

    somewhat working builtins call by excluding non-piped builtins from piped execution.

--- source/execute.c
+++ source/execute.c
@@ -43,37 +43,33 @@ int		execute_builtin(t_cmd *cmd, t_data *data)
 
 int		is_builtin(t_cmd *cmd, t_data *data)
 {
-	//printf("builtin\n");
 	if (ft_strncmp(cmd->cmd, "echo", ft_strlen(cmd->cmd) + 1) == 0)
-	{
-		return (1);
-	}
+		return (0);
 	else if (ft_strncmp(cmd->cmd, "cd", ft_strlen(cmd->cmd) + 1) == 0)
-    {
 		return (1);
-	}
-	else if (ft_strncmp(cmd->cmd, "pwd", ft_strlen(cmd->cmd) + 1) == 0)
-	{
-		// ft_pwd(data);
+	// else if (ft_strncmp(cmd->cmd, "pwd", ft_strlen(cmd->cmd) + 1) == 0)
+	// 	return (1);
+	else if (ft_strncmp(cmd->cmd, "env", ft_strlen(cmd->cmd) + 1) == 0)
 		return (1);
-	}
-	/*else if (ft_strncmp(cmd->cmd, "env", ft_strlen(cmd->cmd) + 1) == 0)
-        ft_env(data);
-    else if (ft_strncmp(cmd->cmd, "exit", ft_strlen(cmd + 1)) == 0)
-        exit(0);
-    else if (ft_strncmp(cmd->cmd, "export", ft_strlen(cmd + 1)) == 0)
-        ft_export(cmd, data);
-	else if (ft_strncmp(cmd->cmd, "unset", ft_strlen(cmd + 1)) == 0)
-		ft_unset(cmd, data);*/
+    else if (ft_strncmp(cmd->cmd, "exit", ft_strlen(cmd->cmd) + 1) == 0)
+		return (1);
+    else if (ft_strncmp(cmd->cmd, "export", ft_strlen(cmd->cmd) + 1) == 0)
+		return (1);
+	else if (ft_strncmp(cmd->cmd, "unset", ft_strlen(cmd->cmd) + 1) == 0)
+		return (1);
 	return (0);
 }
+
 void	execute_cmd(t_cmd *cmd, t_data *data)
 {
+	if (cmd->type == EMPTY)
+	{
+		set_fd(cmd);
+		return ;
+	}
 	if (is_builtin(cmd, data) == 1)
 	{
-		set_pipes(data, cmd);
 		set_fd(cmd);
-		close_pipes(data->cmd_list, NULL);
 		execute_builtin(cmd, data);
 	}
 	else
@@ -81,7 +77,11 @@ void	execute_cmd(t_cmd *cmd, t_data *data)
 		cmd->pid = fork();
 		if (cmd->pid == 0)
 		{
-			exec_cmd(cmd, data);
+			set_pipes(data, cmd);
+			set_fd(cmd);
+			close_pipes(data->cmd_list, NULL);
+			if (!execute_builtin(cmd,data))
+				exec_cmd(cmd, data);
 			free_data(data);
 			free(data->token_root);
 			free(data->cmd_list);
