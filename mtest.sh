080a8a847667df87f4ca028ab67f3437e2931e1d
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-09-12 15:50:05 +0200

    minor improvement to tester

--- mtest.sh
+++ mtest.sh
@@ -1,3 +1,4 @@
+# Performs a simple diff between the bash inline output and the mshell inline output
 RED="\033[31m"
 GREEN="\033[32m"
 NC="\033[0m"
@@ -12,6 +13,11 @@ NC="\033[0m"
 # 	color=$RED
 # fi
 # printf "The program compiles:	${color}[%s]\n${NC}" $result
+if ! [[ -v 1 ]];
+then 
+	echo "Use with ./mtests.sh [Cmd to be tested]"
+	exit 1;
+fi
 bash -c "$1" > ./tmp_b
 ./mshell -c "$1" > ./tmp_m
 diff ./tmp_b ./tmp_m
