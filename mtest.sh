314626041b493c815e876303b60b13e5b54728ea
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-09-12 19:36:45 +0200

    slight changes to mtest.sh

--- mtest.sh
+++ mtest.sh
@@ -2,26 +2,12 @@
 RED="\033[31m"
 GREEN="\033[32m"
 NC="\033[0m"
-# make
-# exit_code=$?
-# if [ $exit_code -eq 0 ]
-# then
-# 	result="OK"
-# 	color=$GREEN
-# else
-# 	result="KO"
-# 	color=$RED
-# fi
-# printf "The program compiles:	${color}[%s]\n${NC}" $result
 if ! [[ -v 1 ]];
 then 
 	echo "Use with ./mtests.sh [Cmd to be tested]"
 	exit 1;
 fi
-echo > ./tmp_b
-echo > ./tmp_m
-echo > ./err_b
-echo > ./err_m
+echo > ./tmp_b ; echo > ./tmp_m ; echo > ./err_b ; echo > ./err_m
 bash -c "$1" > ./tmp_b 2> ./err_b
 exit_code_b=$?
 ./mshell -c "$1" > ./tmp_m 2> ./err_m
