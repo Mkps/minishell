629ae3c7d6372638d1d223a270f962c7caec139a
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-09-12 16:46:44 +0200

    added error output testing to mtest

--- mtest.sh
+++ mtest.sh
@@ -20,9 +20,11 @@ then
 fi
 echo > ./tmp_b
 echo > ./tmp_m
-bash -c "$1" > ./tmp_b
+echo > ./err_b
+echo > ./err_m
+bash -c "$1" > ./tmp_b 2> ./err_b
 exit_code_b=$?
-./mshell -c "$1" > ./tmp_m
+./mshell -c "$1" > ./tmp_m 2> ./err_m
 exit_code_m=$?
 diff ./tmp_b ./tmp_m
 exit_code=$?
@@ -34,7 +36,18 @@ else
 	result="KO"
 	color=$RED
 fi
-printf "Output matches bash:	${color}[%s]\n${NC}" $result
+printf "Standard output matches bash:	${color}[%s]\n${NC}" $result
+diff ./err_b ./err_m
+exit_code=$?
+if [ $exit_code -eq 0 ]
+then
+	result="OK"
+	color=$GREEN
+else
+	result="KO"
+	color=$RED
+fi
+printf "Error output matches bash:	${color}[%s]\n${NC}" $result
 if [ $exit_code_b -eq $exit_code_m ]
 then
 	result="OK"
@@ -43,5 +56,5 @@ else
 	result="KO"
 	color=$RED
 fi
-printf "Exit code matches bash:	${color}[%s]\n${NC}" $result
-rm ./tmp_b ./tmp_m
+printf "Exit code matches bash:		${color}[%s]\n${NC}" $result
+rm ./tmp_b ./tmp_m ./err_b ./err_m
