64b52a23f6445dcbd1da4c6c3526fb85b6ae2d92
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-09-13 20:14:25 +0200

    add history now doesnt take beginning ws or ws only input

--- source/lexer.c
+++ source/lexer.c
@@ -89,6 +89,12 @@ int	ft_get_token(char *input, t_data *data)
 	}
 	return (i);
 }
+int	ft_is_ws(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
 
 //Scan input for tokens then load them into the pre-command table.
 int	scan_input(t_data *data)
@@ -98,11 +104,11 @@ int	scan_input(t_data *data)
 	char *input;
 	int	input_length;
 
-
 	input = data->user_input;
 	data->parse_status = NONE;
 	i = 0;
-	if (input == NULL)
+	while (*input && ft_is_ws(*input)) input++;
+	if (input == NULL || *input == 0)
 		return (EXIT_FAILURE);
 	add_history(input);
 	input_length = ft_strlen(input);
