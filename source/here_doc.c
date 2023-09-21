710bc8e17aa73fb6085c1d2b579824b4339c5c8b
Alexis LOUBIERE <aloubier@student.42.fr>
2023-09-21 12:04:02 +0200

    working here_doc var_expander

--- source/here_doc.c
+++ source/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: aloubier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:21:08 by aloubier          #+#    #+#             */
-/*   Updated: 2023/09/20 09:25:58 by aloubier         ###   ########.fr       */
+/*   Updated: 2023/09/21 12:03:34 by aloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,101 @@
 
 #define HEREDOC_WARNING_EOF "minishell: warning: here-document delimited by end-of-file (wanted `"
 /** Gets the input */
+int		get_flag(char *limiter)
+{
+	int	i;
+	int	quote_status;
+
+	quote_status = 0;
+	i = -1;
+	while (limiter[++i]) 
+	{
+		if (limiter[i] == '\'')
+			quote_status++;
+	}
+	return (!(quote_status % 2 == 0));
+}
+
+char	*heredoc_var_expand(t_data *data, char *str)
+{
+	int		i;
+	int		n;
+	char	*ret;
+	char	*tmp;
+	char	*tmp_str;
+	char	*exit_code;
+	
+	i = 0;
+	n = 1;
+	ret = ft_strdup(str);
+	exit_code = ft_itoa(g_exit_code);
+	while (ret[i])
+	{
+		if (ret[i] == '$')
+		{
+			if (ft_isalpha(ret[i + 1]) || ret[i + 1] == '_')
+			{
+				n = 1;
+				while (*(ret + i + n) && (ft_isalnum(ret[i + n]) || ret[i + n] == '_'))
+					n++;
+				if (n != 1)
+				{
+					tmp = ret;
+					tmp_str = ft_str_extract(ret + i + 1, n - 1);
+					ret = str_replace(ret, i, n, get_var(data, tmp_str));
+					free(tmp_str);
+					free(tmp);
+				}
+				i = 0;
+			}
+			else if (ft_isdigit(ret[i + 1]))
+			{
+				tmp = ret;
+				ret = str_replace(ret, i, 2, "");
+				free(tmp);
+				i = 0;
+			}
+			else if (ret[i + 1] == '?')
+			{
+				tmp = ret;
+				ret = str_replace(ret, i, 2, exit_code);
+				free(tmp);
+				i = 0;
+			}
+			else
+				i++;
+		}
+		else
+			i++;
+	}
+	free(exit_code);
+	free(str);
+	return (ret);
+}
+
 void	here_doc_input(t_data *data, char *limiter, int *fd)
 {
 	char	*str;
 	char	*tmp;
+	int		flag;
 
 	signals_here_doc_child();
 	signal(SIGQUIT, (void (*) (int))here_doc_child_SIGINT);
 	here_doc_child_SIGINT(42, fd);
 	close(fd[0]);
+	flag = get_flag(limiter);
 	str = "str";
 	while (str)
 	{
 		str = readline("heredoc> ");
 		if (str)
+		{
 			str = ft_strappend(str, "\n", 2);
-		if (str == NULL || (!ft_strncmp(str, limiter, ft_strlen(limiter)) 
-				&& (ft_strlen(limiter) == ft_strlen(str) - 1)))
+			if (!flag)
+				str = heredoc_var_expand(data, str);
+		}
+		if (str == NULL || (!ft_strncmp(str, limiter + flag, ft_strlen(limiter + flag)) 
+				&& (ft_strlen(limiter + flag) == ft_strlen(str) - 1)))
 		{
 			free(limiter);
 			close(fd[1]);
